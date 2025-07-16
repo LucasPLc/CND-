6181fe1092f7572c285f62115b0108be8afbfbd4
LucasCardoso54 <lucas.cardoso@saamauditoria.com.br>
2025-07-16 08:27:39 -0300

    Remove pasta SAAM-API-CND para reconfigurar como submódulo

--- SAAM-API-CND/src/main/java/br/com/sisaudcon/saam/saam_sped_cnd/domain/exception/AcessoNegadoException.java
+++
@@ -1,5 +0,0 @@
-package br.com.sisaudcon.saam.saam_sped_cnd.domain.exception;
-
-public class AcessoNegadoException extends RuntimeException {
-    public AcessoNegadoException(String msg) { super(msg); }
-}
--- SAAM-API-CND/src/main/java/br/com/sisaudcon/saam/saam_sped_cnd/domain/exception/ClienteNaoAutorizadoException.java
+++
@@ -1,7 +0,0 @@
-package br.com.sisaudcon.saam.saam_sped_cnd.domain.exception;
-
-public class ClienteNaoAutorizadoException extends RuntimeException {
-    public ClienteNaoAutorizadoException(String msg) {
-        super(msg);
-    }
-}
